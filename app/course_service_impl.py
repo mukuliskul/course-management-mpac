85d685a5995cd0e7d6d19380186c2f6fb5056fb4
Mukul Sharma <mukulsharma.jan11@gmail.com>
2024-02-26 19:16:58 -0500

    Added appropriate exception handling and Student methods

--- app/course_service_impl.py
+++ app/course_service_impl.py
@@ -1,54 +1,57 @@
 from typing import List
 from app.course_service import CourseService
 
-# using a manual id generator for simplicity rather than using uuid
+# using a manual id generator for simplicity and readability rather than using uuid
+# dictionary operations are wrapped in try-catch block to override with custom exceptions
 
 class CourseServiceImpl(CourseService):
   def __init__(self):
     self.courses = {} # {course_id : course_object}
-    self.id_generator = 0
+    self.course_id = 0 # course id generator
 
+  #TODO: SHOW ALL DETAILS ABOUT COURSE
   def get_courses(self):
     if self.courses:
-      return [course.name for course in self.courses.values()]
+      # returning course.name as the dictionary stores course object in value
+      return [course.name for course in self.courses.values()] 
     else:
       raise LookupError("No courses found")
   
+  #TODO: SHOW ALL DETAILS ABOUT COURSE
   def get_course_by_id(self, course_id):
-    if self.courses[course_id]:
-      return self.courses[course_id].name
-    else:
+    try:
+      course = self.courses[course_id]
+      return course
+    except KeyError:
        raise KeyError(f"Course with ID {course_id} not found")
+     
+  #TODO : might remove this function
+  def get_students_by_course_id(self, course_id):
+    course = self.get_course_by_id(course_id)
+    return [student.id for student in course.students_enrolled]
   
   def create_course(self, course_name):
     new_course = Course(course_name)
-    self.courses[self.id_generator] = new_course
-    self.id_generator+=1
+    self.courses[self.course_id] = new_course
+    self.course_id+=1
   
   def delete_course(self, course_id):
-    if self.courses[course_id]:
-      self.courses.pop(course_id)
-    else:
-      raise KeyError(f"Course with ID {course_id} not found")
+    course = self.get_course_by_id(course_id)
+    self.courses.pop(course_id)
 
   def create_assignment(self, course_id, assignment_name):
-    """
-    Creates a new assignment for a course.
-    """
-    pass
+    course = self.get_course_by_id(course_id)
+    course.create_course_assignment(assignment_name)
 
   def enroll_student(self, course_id, student_id):
-    """
-    Enrolls a student in a course.
-    """
-    pass
+    course = self.get_course_by_id(course_id)
+    course.enroll_student_in_course(student_id)
+    
 
   def dropout_student(self, course_id, student_id):
-    """
-    Drops a student from a course.
-    """
-    pass
-
+    course = self.get_course_by_id(course_id)
+    course.drop_student_from_course(student_id, course_id) 
+  
   def submit_assignment(self, course_id, student_id, assignment_id, grade: int):
     """
     Submits an assignment for a student. A grade of an assignment will be an integer between 0 and 100 inclusive.
@@ -75,17 +78,39 @@ class CourseServiceImpl(CourseService):
 
 class Course():
   def __init__(self, course_name):
-    self.id = "course_id" #auto-generated
     self.name = course_name
-    self.details = {"student-id" : "assignment-id"} #added by a setter  
-
-class Student():
-  def __init__(self, student_id, course_id, assignment_id):
-    self.id = student_id
-    self.course_ids = ["course_id"] #added by a setter
-  
-class Assignments():
-  def __init__(self, assignment_id, assignment_name, assignment_grade):
-    self.id = assignment_id
+    self.assignments = {} # {assignment_id : assignment_object}
+    self.assignment_id = 0 # assignment id generator
+    self.students_enrolled = []
+    
+  def create_course_assignment(self, assignment_name):
+    new_assignment = Assignment(assignment_name)
+    self.assignments[self.assignment_id] = new_assignment
+    new_assignment += 1 
+    
+  def enroll_student_in_course(self, student_id):
+    new_student = Student(student_id)
+    self.students_enrolled.append(new_student)
+    
+  def drop_student_from_course(self, student_id, course_id):
+    remove_student = None # student to be removed
+    for student in self.students_enrolled:
+      if student.id == student_id:
+        remove_student = student
+        break
+    
+    if(remove_student):
+      self.students_enrolled.remove(remove_student)
+    else:
+      raise ValueError(f"Student id: {student_id} not found in Course: {self.name} (id:{course_id})")
+    
+class Assignment():
+  def __init__(self, assignment_name, assignment_grade = None):
     self.name = assignment_name
     self.grade = ["assignment-grades"] 
+    
+class Student():
+  def __init__(self, student_id):
+    self.id = student_id
+    #self.course_ids = ["course_id"] #added by a setter
+  
--- main.py
+++ main.py
@@ -4,12 +4,13 @@ if __name__ == "__main__":
   course_service = CourseServiceImpl()
 
   # Start receiving requests...  
+  
+  # running course CRUD
   course_service.create_course("Testing0")
   course_service.create_course("Testing1")
   course_service.create_course("Testing2")
   
   print(course_service.get_courses())
-  
   print(course_service.get_course_by_id(2))
   
   course_service.delete_course(1)
@@ -17,7 +18,30 @@ if __name__ == "__main__":
   print(course_service.get_courses())
   
   course_service.create_course("Testing3")
-  
   course_service.create_course("Testing4")
   
   print(course_service.get_courses())
+  
+  # running student CRUD
+  
+  mylsit = [100, 200, 300]
+  
+  course_service.enroll_student(0, 000)
+  course_service.enroll_student(0, 100)
+  course_service.enroll_student(0, 200)
+  
+  course_service.enroll_student(2, 222)
+  course_service.enroll_student(2, 223)
+  course_service.enroll_student(2, 224)
+  course_service.enroll_student(2, 225)
+  course_service.enroll_student(2, 226)
+  
+  print(course_service.get_students_by_course_id(0))
+  print(course_service.get_students_by_course_id(2))
+  
+  course_service.dropout_student(0, 100)
+  course_service.dropout_student(2, 222)
+  course_service.dropout_student(3, 0)
+  
+  print(course_service.get_students_by_course_id(0))
+  print(course_service.get_students_by_course_id(2))
